2c1cee8e76fefa8a289b77a804eaebf321e58e99
Syamlal CM <syamjayaraj@gmail.com>
2020-10-04 12:42:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
+    };
+  }
+
+  componentDidMount() {
+    return fetch(`https://techomoro.com/wp-json/wp/v2/posts/`)
+      .then((response) => response.json())
+      .then((responseJson) => {
+        this.setState({
+          posts: responseJson,
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.posts.map((item, index) => {
+          return (
+            <div>
+              <div class="row">
+                <div class="leftcolumn">
+                  <div class="card">
+                    <div className="title">
+                      <h1>{item.title.rendered}</h1>
+                    </div>
+                    <div
+                      className="content"
+                      dangerouslySetInnerHTML={{
+                        __html: item.content.rendered,
+                      }}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
